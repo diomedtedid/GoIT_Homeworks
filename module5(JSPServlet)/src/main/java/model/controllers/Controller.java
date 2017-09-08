e0e14ee214eec97c734d71d3937bc82b37269940
troy <vitalii.proskura@gmail.com>
2017-09-09 00:02:38 +0300

    module5 - simple servlet and jsp Controller - done

---
+++ module5(JSPServlet)/src/main/java/model/controllers/Controller.java
@@ -0,0 +1,84 @@
+package model.controllers;
+
+import model.dao.TaskDao;
+import model.dao.UserDao;
+import model.dao.inmemorydaoimpl.UserMemoryDaoImpl;
+import model.domain.Task;
+import model.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by User on 08.09.2017.
+ */
+public class Controller {
+    private UserDao userDao;
+    private TaskDao taskDao;
+    private Controller controller;
+
+    private Controller() {
+    }
+
+    public Controller getController() {
+
+        if (controller == null) {
+            this.controller = new Controller();
+        }
+
+        return this.controller;
+    }
+
+    public List<Task> getTaskListByUser (User user) {
+        List<Task> taskList = new ArrayList<>();
+
+        if (userDao.readAll().contains(user)) {
+            taskList.addAll(taskDao.readAll()
+                    .stream()
+                    .filter(task -> task.getUser().equals(user))
+                    .collect(Collectors.toList())
+            );
+        } else {
+            userDao.create(user);
+        }
+
+        return taskList;
+    }
+
+    public void saveUser (User user) {
+        userDao.create(user);
+    }
+
+    public void saveTask (Task task) {
+        if (task.getUser() == null || task.getDateOfStart() == null || task.getDateOfEnd() == null) {
+            throw  new IllegalArgumentException("New task contains null fields");
+        }
+
+        this.taskDao.create(task);
+    }
+
+    public void updateTask(Task task) {
+        taskDao.update(task);
+    }
+
+    public void deleteTask(Task task) {
+        taskDao.delete(task);
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public TaskDao getTaskDao() {
+        return taskDao;
+    }
+
+    public void setTaskDao(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+}
